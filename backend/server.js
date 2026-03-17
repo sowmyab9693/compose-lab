cafe1290c0da7a6a9b980090a0aaa6d2b32fc61f
Sowmya B <sowmyabalabandi@gmail.com>
2026-03-17 13:21:48 +0000

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Backend API 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "API is running" });
+});
+
+app.listen(3000, () => {
+  console.log("Backend API running on port 3000");
+});
