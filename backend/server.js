f38f634f543bca136926bd3c0c4b29ff9a637675
Sowmya B <sowmyabalabandi@gmail.com>
2026-03-27 13:17:31 +0000

    test auto deply

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Backend API 🚀");
+  res.send("Deployed via CI/CD 🚀");
 });
 
 app.get("/health", (req, res) => {
